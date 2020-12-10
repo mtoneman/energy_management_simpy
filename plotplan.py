61c15a667ff29ba9771411d4a031f48609d0d1bf
Michiel Toneman <michiel.git@kopz.org>
2020-12-10 16:27:09 +0100

    Many additions and fixes

--- plotplan.py
+++ plotplan.py
@@ -9,14 +9,10 @@ import pandas as pd
 app = dash.Dash('Hello World',
                 external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
 
-#df = pd.read_csv('energy.csv')
-#fig = px.line(df, x="date", y="energylevel",labels=dict(energylevel='Energy Level',date='Date'))
-#fig = px.line(df, x="date", y="freshwaterlevel",labels=dict(freshwaterlevel='Fresh Water Level',date='Date'))
-#fig.update_yaxes(range=[0, 100])
-
 config = { "energylevel": { "label": "Energy Level (%)", "range": [0,100]},
            "batterylevel": { "label": "Battery Level (kWh)", "range": [0,5000]},
-           "freshwaterlevel": { "label": "Fresh Water Level (l)", "range": [0,10000]}
+           "freshwaterlevel": { "label": "Fresh Water Level (l)", "range": [0,10000]},
+           "solarenergy": { "label": "Solar Power Generated (kWh)", "range": [0,50]}
 	 }
 
 app.layout = html.Div([
@@ -25,7 +21,8 @@ app.layout = html.Div([
         options=[
             {'label': config['energylevel']['label'], 'value': 'energylevel'},
             {'label': config['batterylevel']['label'], 'value': 'batterylevel'},
-            {'label': config['freshwaterlevel']['label'], 'value': 'freshwaterlevel'}
+            {'label': config['freshwaterlevel']['label'], 'value': 'freshwaterlevel'},
+            {'label': config['solarenergy']['label'], 'value': 'solarenergy'}
         ],
         value='energylevel'
     ),
@@ -38,25 +35,10 @@ app.layout = html.Div([
 ], style={'width': '500'})
 
 
-#marker = dict(color = ['rgba(63, 195, 128, 1)' if x>0 else 'rgba(219, 10, 91, 1)' for x in df["percentageChange30dBtc"]], line = dict(color='rgb(0,0,0)',width=1.5)),
-
-#
-#dcc.Interval(
-#            id='interval-component',
-#            interval=1*1000, # in milliseconds
-#            n_intervals=0
-
 
 @app.callback(Output('my-graph', 'figure'), [Input('my-dropdown', 'value'),Input('interval-component', 'n_intervals')])
 def update_graph(selected_dropdown_value, n_intervals):
     df = pd.read_csv('energy.csv')
-    #df = pd.DataFrame(dict(market=[4, 2, 5]))
-    #web.DataReader(
-    #    selected_dropdown_value,
-    #    'yahoo',
-    #    dt(2017, 1, 1),
-    #    dt.now()
-    #)
 
     if(selected_dropdown_value == 'energylevel'):
         return {
@@ -67,7 +49,6 @@ def update_graph(selected_dropdown_value, n_intervals):
 	    "hovertemplate": "%{x}<br>%{y}%<extra></extra>",
 	    "showlegend": False,
     	    "line": {
-                 #"color": "#636efa",
                  "color": "#63da6e",
                  "width": "4",
                  "dash": "solid"
@@ -102,19 +83,31 @@ def update_graph(selected_dropdown_value, n_intervals):
     	    "yaxis": { 
     	               "title": { "text": config[selected_dropdown_value]['label'] },
     	               "range": config[selected_dropdown_value]['range']
-                           #"autorange": "true"
                          },
     	    }
-        	#"title": {"text": "A Figure Specified By Python Dictionary"},
+        }
+    elif(selected_dropdown_value == 'solarenergy'):
+        return {
+            'data': [{
+                'x': df.date,
+                'y': df[selected_dropdown_value],
+    	    "type": "bar",
+            }],
+            'layout': {
+    	    'margin': {'l': 60, 'r': 10, 't': 20, 'b': 30},
+    	    "yaxis": { 
+    	               "title": { "text": config[selected_dropdown_value]['label'] },
+                       "autorange": "true"
+                      },
+    	    }
         }
     else:
-        return {
+         return {
             'data': [{
                 'x': df.date,
                 'y': df[selected_dropdown_value],
     	    "type": "scatter",
     	    "line": {
-                 #    "color": "#636efa",
                      "width": "4",
                      "dash": "solid"
                 },
@@ -124,17 +117,11 @@ def update_graph(selected_dropdown_value, n_intervals):
     	    "yaxis": { 
     	               "title": { "text": config[selected_dropdown_value]['label'] },
     	               "range": config[selected_dropdown_value]['range']
-                           #"autorange": "true"
                          },
-    	}
+    	    }
     	#"title": {"text": "A Figure Specified By Python Dictionary"},
         }
  
-
-
-#app.layout = html.Div([
-#    dcc.Graph(figure=fig)
-#], style={'width': '500'})
 
 
 if __name__ == '__main__':
--- readplan.py
+++ readplan.py
@@ -12,18 +12,18 @@ FRESHWATER_CAPACITY_L = 10000
 FRESHWATER_START_L = 6000
 
 SIM_TIME = 60*24*15 # Simulation time in minutes, 15 days
-#SIM_TIME = 60 # Simulation time in minutes, ten days
 
 start_time = 0
-battery_level = BATTERY_START_KWH
 
 #compliment {"crew_only","crew_and_owner", "full_charter"}
 #current_passengers = "full_charter"
+
 schedule = []
+forecast = []
 energy = []
 
 def row_at_time(time, table):
-    timelist = [li[0] for li in table]
+    timelist = [li["minutes"] for li in table]
     timelist.append(1000000000)
   
     # using enumerate() + next() to find index of 
@@ -32,60 +32,113 @@ def row_at_time(time, table):
 
     return max(0,res);
 
+def time_at_minute(minutes, starttime):
+    return starttime + timedelta(minutes=minutes)
+
 def generate_freshwater(env, freshwater_tank):
+    yield env.timeout(21) #small offset for prettier graph
     while True:
         yield env.timeout(30)
         if freshwater_tank.level < 2000:
-            print('starting a freshwater generation cycle at %.2f.' % (env.now))
+            #print('starting a freshwater generation cycle at %.2f.' % (env.now))
             while freshwater_tank.level < 9000:
                 freshwater_tank.put(1000) # 5Wh per liter
                 battery.get(5)
                 yield env.timeout(30)
 
 def laundry(env, battery, cycle_time):
-    print('starting a laundry cycle of %d minutes at %.2f.' % (cycle_time, env.now))
+    #print('starting a laundry cycle of %d minutes at %.2f.' % (cycle_time, env.now))
     yield env.timeout(cycle_time/2)
     battery.get(4) # washing cycle uses 4kWh 
     yield env.timeout(cycle_time/2)
     battery.get(5) # drying cycle uses 5kWh 
-    print('finished the laundry cycle at %.2f.' % (env.now))
+    #print('finished the laundry cycle at %.2f.' % (env.now))
 
 def use_freshwater(env, freshwater_tank):
     """Simulate water use"""
     freshwater_use_lph = {"crew_only" : 200, "crew_and_owner": 350, "full_charter": 1000}
     while True:
-        compliment = schedule[row_at_time(env.now, schedule)][1]
-        freshwater_tank.get(freshwater_use_lph[compliment])
-        print('fresh water at %d' % (freshwater_tank.level))
+        compliment = schedule[row_at_time(env.now, schedule)]["compliment"]
+ 
+        # daylight hours only
+        time_now = time_at_minute(env.now, start_time) 
+        factor = 1
+        if(time_now.hour < 6):
+            factor = 0.2
+        elif(time_now.hour < 9):
+            factor = 1
+        elif(time_now.hour < 17):
+            factor = 0.6
+        elif(time_now.hour < 22):
+            factor = 1
+        else:
+            factor = 0.2
+
+        freshwater_tank.get(freshwater_use_lph[compliment] * factor)
+        #print('fresh water at %d' % (freshwater_tank.level))
         yield env.timeout(60)
+
+def solar_egeneration(env, battery):
+    """Simulate solar e-generation"""
+    yield env.timeout(9) #small offset for prettier graph
+    power_generated_ph = 12 # average for 12 daylight hours
+    while True:
+        yield env.timeout(60)
+        power_gen = power_generated_ph 
+
+        # sailing reduces the effective power to 60%
+        activity = schedule[row_at_time(env.now, schedule)]["activity"]
+        if(activity == "sailing"):
+            power_gen = power_gen * 0.6
+
+        # factor in the forecast solar energy
+        solar_energy = float(forecast[row_at_time(env.now, forecast)]["solar_energy"])
+        power_gen = power_gen * (min(solar_energy,5.0) / 5.0)
+
+        # daylight hours only
+        time_now = time_at_minute(env.now, start_time) 
+        if(time_now.hour < 7 or time_now.hour > 19):
+            power_gen = 0
+
+	# daylight cycle
+        power_gen = power_gen * (3 - (abs(13 - time_now.hour)* abs(13 - time_now.hour)) / 12)
+
+        if(power_gen > 0):
+            solar.put(power_gen)
+
+        if power_gen > 0 and battery.level < (BATTERY_CAPACITY_KWH - power_gen):
+            #print('solar panels generated %f at %s' % (power_gen, time_now))
+            battery.put(power_gen)
+
 
 def drag_egeneration(env, battery):
     """Simulate drag e-generation"""
-    power_generated_ph = 90
+    yield env.timeout(7) #small offset for prettier graph
+    power_generated_ph = 100
     while True:
         yield env.timeout(60)
-        compliment = schedule[row_at_time(env.now, schedule)][2]
-        #if compliment == 'sailing' and 100 * (battery.level / BATTERY_CAPACITY_KWH) < 80 and  battery.level < (BATTERY_CAPACITY_KWH - power_generated_ph):
-        if compliment == 'sailing' and battery.level < (BATTERY_CAPACITY_KWH - power_generated_ph):
+        activity = schedule[row_at_time(env.now, schedule)]["activity"]
+        if activity == 'sailing' and battery.level < (BATTERY_CAPACITY_KWH - power_generated_ph):
             battery.put(power_generated_ph)
 
 
 def laundry_scheduler(env, battery):
+    yield env.timeout(17) #small offset for prettier graph
     """Schedule laundry cycles"""
     cycle_time = 180
     cycle_schedule_hours = {"crew_only" : 48, "crew_and_owner": 24, "full_charter": 12}
     while True:
-        compliment = schedule[row_at_time(env.now, schedule)][1]
+        compliment = schedule[row_at_time(env.now, schedule)]["compliment"]
         next_cycle = 60 * cycle_schedule_hours[compliment] - cycle_time
-        print('schedule next laundry cycle for %s in %d minutes' % (compliment,next_cycle))
+        #print('schedule next laundry cycle for %s in %d minutes' % (compliment,next_cycle))
         env.process(laundry(env, battery, cycle_time))
         yield env.timeout(next_cycle)
 
 def undefined_load(env, battery):
     """Placeholder Electrical Load Simulator"""
-    undefined_load_kWh = {"crew_only" : 30, "crew_and_owner": 40, "full_charter": 80}
+    undefined_load_kWh = {"crew_only" : 30, "crew_and_owner": 40, "full_charter": 70}
     while True:
-        compliment = schedule[row_at_time(env.now, schedule)][1]
+        compliment = schedule[row_at_time(env.now, schedule)]["compliment"]
         battery.get(undefined_load_kWh[compliment]/4)
         yield env.timeout(15)
 
@@ -94,9 +147,12 @@ def es(env, energy, start_time):
     while True:
         reporting_cycle = 60
         yield env.timeout(reporting_cycle)
-        energy.append({"date": start_time + timedelta(minutes=env.now), "batterylevel": battery.level, "freshwaterlevel": freshwater_tank.level, 
+        energy.append({"date": start_time + timedelta(minutes=env.now), "batterylevel": battery.level, "freshwaterlevel": freshwater_tank.level, "solarenergy": solar.level, 
            "energylevel": ((battery.level/BATTERY_CAPACITY_KWH)*9 + (freshwater_tank.level/FRESHWATER_CAPACITY_L) *1)  * 100/10 })        
-        print('\t Battery level at %d kWh at %d' %  (battery.level,env.now ))
+        #print('\t Battery level at %d kWh at %d' %  (battery.level,env.now ))
+        slevel = solar.level
+        if slevel != 0:
+            solar.get(slevel)
 
 with open('plan.csv', mode='r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
@@ -108,27 +164,49 @@ with open('plan.csv', mode='r') as csv_file:
         if line_count == 1:
             start_time = datetime.fromisoformat(row["datetime"]);
             print(f'\tstart time {start_time}')
-        print(f'\tRow {line_count} at date {row["datetime"]}: {row["activity"]} at/to {row["destination_city"]},{row["destination_country"]}.')
+        #print(f'\tRow {line_count} at date {row["datetime"]}: {row["activity"]} at/to {row["destination_city"]},{row["destination_country"]}.')
         row_time = datetime.fromisoformat(row["datetime"])
+        schedule_row = {"minutes":(row_time - start_time).total_seconds()/60,"compliment":row["compliment"],"activity":row["activity"],"destination_city":row["destination_city"],"destination_country":row["destination_country"]}
+        print(schedule_row)
+        schedule.append(schedule_row)
+        line_count += 1
+    print(f'Processed {line_count} lines.')
+ 
+with open('forecast.csv', mode='r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    line_count = 0
+    format_str = '%m/%d/%Y %H:%M:%S' # the Date Time format
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        row_time = datetime.strptime(row["Date time"], format_str)
         #print((row_time - start_time).total_seconds()/60)
-        schedule.append([(row_time - start_time).total_seconds()/60,row["compliment"],row["activity"],row["destination_city"],row["destination_country"]])
+        forecast_row = {"minutes":(row_time - start_time).total_seconds()/60,"location":row["Name"],"wind_direction":row["Wind Direction"],"solar_energy":row["Solar Energy"],"cloud_cover":row["Cloud Cover"],"wind_speed":row["Wind Speed"]}
+        print(forecast_row)
+        forecast.append(forecast_row)
         line_count += 1
     print(f'Processed {line_count} lines.')
         
+       
 
 env = simpy.Environment()
 
+solar = simpy.Container(env, 100000000, init=0)
 battery = simpy.Container(env, BATTERY_CAPACITY_KWH, init=BATTERY_START_KWH)
 freshwater_tank = simpy.Container(env, FRESHWATER_CAPACITY_L, init=FRESHWATER_START_L)
 
 fwg = simpy.Resource(env, capacity=1)
 
+#consumers
 env.process(es(env, energy, start_time))
 env.process(laundry_scheduler(env, battery))
 env.process(use_freshwater(env, freshwater_tank))
 env.process(generate_freshwater(env, freshwater_tank))
 env.process(undefined_load(env, battery))
 
+#generators
+env.process(solar_egeneration(env, battery))
 env.process(drag_egeneration(env, battery))
 
 env.run(until=SIM_TIME)
